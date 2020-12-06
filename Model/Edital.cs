bcbed6f5f12aeaa46bb35fdfc15658c0fcc933b8
emersonv25 <emersondejesussantos@hotmail.com>
2020-12-06 15:51:57 -0300

    add construtores

--- Model/Edital.cs
+++ Model/Edital.cs
@@ -6,6 +6,38 @@ namespace edital.Model
 {
     public class Edital
     {
+    
+        public Edital()
+        { 
+
+        }
+
+        public Edital(string nome, int vigencia, DateTime datainicio, DateTime datafim){
+            this.nome = nome;
+            this.vigencia = vigencia;
+            this.datainicio = datainicio;
+            this.datafim = datafim;
+        }
+
+        public Edital(int id, string nome, int vigencia, DateTime datainicio, DateTime datafim){
+            this.id = id;
+            this.nome = nome;
+            this.vigencia = vigencia;
+            this.datainicio = datainicio;
+            this.datafim = datafim;
+        }
+
+        public Edital(int id, string nome, int vigencia, DateTime datainicio, DateTime datafim, List<Segmento> segmentos){
+            this.id = id;
+            this.nome = nome;
+            this.vigencia = vigencia;
+            this.datainicio = datainicio;
+            this.datafim = datafim;
+            this.segmentos = segmentos;
+
+        }
+
+
         [Key]
         public int id { get; set; }
          
--- Model/Inscricao.cs
+++ Model/Inscricao.cs
@@ -6,15 +6,10 @@ namespace edital.Model
     public class Inscricao 
     {
 
-        public int pessoajuridica_id { get; set; }
-
-         
-        public int segmento_id { get; set; }
-         
-        public PessoaJuridica pessoajuridica { get; set; }
-         
-        public Segmento segmento { get; set; }
-         
+        public int pessoajuridica_id { get; set; } 
+        public int segmento_id { get; set; }        
+        public PessoaJuridica pessoajuridica { get; set; }        
+        public Segmento segmento { get; set; }        
         public bool flgativo { get; set; }
         public string nomeiniciativa { get; set; }
         public string objetivos { get; set; }
